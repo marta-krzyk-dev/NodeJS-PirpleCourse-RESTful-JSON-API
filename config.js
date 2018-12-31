e3a25c7b5845fdc7b7a326d809f3bb254e270a8a
Marta Krzyk <marta-krzyk-dev@users.noreply.github.com>
2018-12-31 21:09:37 +0200

    Recognize operating system and set environment

--- config.js
+++ config.js
@@ -21,10 +21,21 @@ environments.production = {
 
 // Determine if production environment was passed as a command line arg, otherwise set to staging
 // In Windows, the arguments are kept in process.argv array
-if (process.argv.length > 2)
-	var environmentToExport = (process.argv.indexOf(environments.production.envName.toLowerCase()) > -1) ? environments.production : environments.staging;
-else 
-	var environmentToExport = environments.staging;
 
-//Export the module
+var environmentToExport;
+
+if (process.platform === "win32")
+{
+	if (process.argv.length > 2)
+		environmentToExport = process.argv.indexOf(environments.production.envName.toLowerCase()) > -1 ? environments.production : environments.staging;
+	else 
+		environmentToExport = environments.staging;
+}
+else
+{
+	var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+	environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+}
+
+// Export the module
 module.exports = environmentToExport;
